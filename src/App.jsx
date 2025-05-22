872a7783063319cfcbaa33df67d44f1c20933903
Favskid <favouroloda24@gmail.com>
2025-05-22 03:32:51 +0100

    configured tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-   <>
-    Hello world
-  </>
+   <div className="min-h-screen flex flex-col">
+      <Navbar />
+    </div>
   );
 }
 
